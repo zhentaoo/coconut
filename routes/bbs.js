a4225f824eb119e19ed8a478a993fca020647d37
zhentaoo <1904974327@qq.com>
2015-08-04 12:18:37 +0800

    发帖页面完成

--- routes/bbs.js
+++ routes/bbs.js
@@ -7,4 +7,23 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.get('/sendArticle', function (req, res, next) {
+    res.render('bbs/article/index', {
+        session: req.session
+    });
+});
+
+router.post('/sendArticle', function (req, res, next) {
+    console.log(req.body.content + req.body.title);
+    var article = require('../model/index').Article;
+    console.log(req.session.name);
+    article.create({
+        title: req.body.title,
+        content: req.body.content,
+        author: req.session.name
+    }, function () {
+        res.redirect('/bbs');
+    });
+});
+
 module.exports = router;
