dfcaa93db340a3825d73e91df526551f18dd9738
zhentaoo <1904974327@qq.com>
2017-01-12 13:45:22 +0800

    add baidu AD

--- src/controller/App.js
+++ src/controller/App.js
@@ -1,5 +1,5 @@
-exports.index = function (req, res, next) {
-    res.render('webrtc/index', {
-        session: req.session
-    });
+exports.index = function(req, res, next) {
+  res.render('webrtc/index', {
+    session: req.session
+  });
 };
--- src/controller/Program.js
+++ src/controller/Program.js
@@ -1,9 +1,8 @@
 var program = require('../model').Program;
 
 exports.index = function (req, res, next) {
-    //res.send(req.session.name);
+   //res.send(req.session.name);
     program.find({}, function (err, docs) {
-            console.log(docs);
             res.render('program/index', {
                 session: req.session,
                 program: docs
@@ -14,7 +13,6 @@ exports.index = function (req, res, next) {
 
 exports.showOneProgram = function (req, res, next) {
     program.findById(req.query.id, function (err, docs) {
-        console.log(docs)
         res.render('program/show.ejs', {
             session: req.session,
             program: docs
