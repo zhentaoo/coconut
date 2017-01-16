077b358bef120fe179fd1d476b66e2a44bbd84ff
zhentaoo <1904974327@qq.com>
2017-01-16 23:06:40 +0800

    sort -1,limit 15,样式修改

--- src/controller/Program.js
+++ src/controller/Program.js
@@ -2,19 +2,22 @@ var program = require('../model').Program;
 
 exports.index = function(req, res, next) {
   //res.send(req.session.name);
-  console.log(req);
-  program.find({}, function(err, docs) {
-    docs.forEach(el => {
-      if (el.content) {
-        el.content = el.content.replace(/<[^>]+>/g,"").slice(0,400).trim();
-      }
-    })
+  // console.log(req);
+  program
+    .find({}, function(err, docs) {
+      docs.forEach(el => {
+        if (el.content) {
+          el.content = el.content.replace(/<[^>]+>/g,"").slice(0,300).trim();
+        }
+      })
 
-    res.render('program/index', {
-      session: req.session,
-      program: docs
-    });
-  })
+      res.render('program/index', {
+        session: req.session,
+        program: docs
+      });
+    })
+    .sort({'date.updateAt': -1})
+    .limit(15);
 };
 
 exports.showOneProgram = function(req, res, next) {
