452faa6d5112f8e1e38f7704b6ad245458bacd15
zhentaoo <1904974327@qq.com>
2017-01-17 20:24:49 +0800

    github oa

--- src/controller/api/auth.js
+++ src/controller/api/auth.js
@@ -1,8 +1,23 @@
+var request = require('request');
+
 exports.index = function (req, res, next) {
     // console.log(res);
-    console.log(req);
+    console.log(req.query);
 
-    res.send({
-      res: req.body
-    });
+    var url = 'https://github.com/login/oauth/access_token';
+    var code = req.query.code;
+    var client_id = 'cd4237027fe35177c068';
+    var client_secret = "aadd141c2d7b2187121159985eab6586bdccf67b";
+
+    request.post({
+        url:url,
+        formData: {
+          code: code,
+          client_id: client_id,
+          client_secret: client_secret
+        }
+      },function (err, res ,body) {
+        res.cookie('github',{code: code, userinfo: body });
+      })
+
   };
